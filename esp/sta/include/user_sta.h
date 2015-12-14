f14e8e9f612c5c107a4a133ceddb52e2a7cf0a17
Jasper <lohuis46@gmail.com>
2015-12-14 20:34:26 +0100

    sta cleaning up

---
+++ esp/sta/include/user_sta.h
@@ -0,0 +1,11 @@
+#ifndef __USER_STA_H__
+#define __USER_STA_H__
+
+#define SSID "Gasten"
+#define WIFI_KEY "welkom08"
+
+void user_sta_init(void); 
+void user_sta_setup_config(void); 
+void user_softap_ipinfo(void);
+
+#endif
---
+++ esp/sta/user/user_sta.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+ *
+ * FileName: user_softap.c
+ *
+ * Description: Setup softap.
+ *
+ * Modification history:
+ *     2015/12/12, v1.0 create this file.
+*******************************************************************************/
+
+#include "osapi.h"
+#include "user_interface.h"
+#include "user_sta.h"
+
+
+void ICACHE_FLASH_ATTR 
+user_sta_init(void)
+{
+	if(wifi_get_opmode() != STATION_MODE) 
+	{
+		os_printf("Setting STATION_MODE\n");
+		wifi_set_opmode(STATION_MODE);
+	}
+
+	wifi_station_set_auto_connect(0);
+	user_sta_setup_config(); 
+
+    //os_printf("Wifi physical level mode: %d \n", (int) wifi_get_phy_mode());
+}
+
+void ICACHE_FLASH_ATTR
+user_sta_setup_config(void)
+{
+	wifi_set_opmode(STATION_MODE);
+	// Build our Access Point configuration details
+	struct station_config config; 
+	wifi_station_get_config(&config);
+
+	os_memset(config.ssid, 0, 32);
+	os_memset(config.password, 0, 64);
+	strncpy(config.ssid, SSID, strlen(SSID));
+	strncpy(config.password, WIFI_KEY, strlen(WIFI_KEY));
+	config.bssid_set = 0; 
+	wifi_station_set_config(&config);
+}
+
+void ICACHE_FLASH_ATTR 
+user_softap_ipinfo(void) 
+{
+	struct ip_info info; 
+	wifi_get_ip_info(SOFTAP_IF, &info);
+	//os_printf("%d, %d, %d \n", info.ip.addr, info.netmask.addr, info.gw.addr); // Getting uint_32 values
+	os_printf("IP: " IPSTR " SUBNET: " IPSTR " GW: " IPSTR " \n", IP2STR(&info.ip), IP2STR(&info.netmask), IP2STR(&info.gw));
+}
