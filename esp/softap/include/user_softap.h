4c4b8bd6a2fa1ee466620e1e0cec3ae3edc169d0
Jasper <lohuis46@gmail.com>
2015-12-14 17:20:13 +0100

    Adding softap setup

---
+++ esp/softap/include/user_softap.h
@@ -0,0 +1,12 @@
+#ifndef __USER_SOFTAP_H__
+#define __USER_SOFTAP_H__
+
+#define SSID "DRUPPEL"
+#define WIFI_KEY "druppelproject"
+
+void user_softap_init(void); 
+void user_softap_setup_config(void); 
+
+//void user_devicefind_init(void);
+
+#endif
---
+++ esp/softap/user/user_softap.c
@@ -0,0 +1,52 @@
+/******************************************************************************
+ *
+ * FileName: user_softap.c
+ *
+ * Description: Setup softap.
+ *
+ * Modification history:
+ *     2015/12/12, v1.0 create this file.
+*******************************************************************************/
+
+#include "osapi.h"
+#include "user_interface.h"
+#include "user_softap.h"
+
+
+void ICACHE_FLASH_ATTR 
+user_softap_init(void)
+{
+	if(wifi_get_opmode() != SOFTAP_MODE) 
+	{
+		os_printf("Setting SOFTAP_MODE\n");
+		wifi_set_opmode(SOFTAP_MODE);
+	}
+
+	user_softap_setup_config(); 
+
+    //os_printf("Wifi mode: %d,  %d \n", wifi_get_opmode(), (int) wifi_get_phy_mode());
+
+
+
+    //char DeviceBuffer[40] = {0};
+
+
+}
+
+void ICACHE_FLASH_ATTR 
+user_softap_setup_config(void)
+{
+	// Build our Access Point configuration details
+	struct softap_config config; 
+	os_strcpy(config.ssid, SSID, strlen(SSID));
+	os_strcpy(config.password, WIFI_KEY, strlen(WIFI_KEY));
+	config.ssid_len = 0;
+	config.channel = 6;
+    //config.authmode = AUTH_WPA2_PSK;
+	config.authmode = AUTH_OPEN;
+	config.ssid_hidden = 0;
+	config.max_connection = 4;
+	//config.beacon_interval = 300; 
+	config.beacon_interval = 100; 
+	wifi_softap_set_config_current(&config);
+}
