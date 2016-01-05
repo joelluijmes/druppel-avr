9264c0938ef7159e537f02e32237544a9f021e07
Joël Luijmes <joelluijmes@gmail.com>
2016-01-05 13:06:02 +0100

    Updated header file

--- util/twi/usi/twi_usi.h
+++ util/twi/usi/twi_usi.h
@@ -5,15 +5,15 @@
 #include <avr/interrupt.h>
 
 #if defined (__AVR_ATtiny85__)
-    #define DDR_USI             DDRB
-    #define PORT_USI            PORTB
-    #define PIN_USI             PINB
-    #define PORT_USI_SDA        PORTB0
-    #define PORT_USI_SCL        PORTB2
-    #define PIN_USI_SDA         PINB0
-    #define PIN_USI_SCL         PINB2
+#define DDR_USI             DDRB
+#define PORT_USI            PORTB
+#define PIN_USI             PINB
+#define PORT_USI_SDA        PORTB0
+#define PORT_USI_SCL        PORTB2
+#define PIN_USI_SDA         PINB0
+#define PIN_USI_SCL         PINB2
 #else
-	#error Device is not supported
+#error Device is not supported
 #endif
 
 typedef uint8_t USIRESULT;
@@ -23,11 +23,9 @@ typedef uint8_t USIRESULT;
 #define USI_SLAVE_RECEIVE 3
 #define TWST_MR_ACK_EXPECTED 4
 
-USIRESULT usi_wait();
-uint8_t usi_write(uint8_t data);
-uint8_t usi_peek();
-uint8_t usi_read();
-void usi_init_master();
-uint8_t usi_init_mt(uint8_t slave_addr);
 USIRESULT usi_init_slave(uint8_t slave_addr);
+uint8_t usi_init_mt(uint8_t slave_addr);
+uint8_t usi_write_master(uint8_t data);
+uint8_t usi_write_slave(uint8_t data);
+uint8_t usi_read_slave();
 void usi_stop();
