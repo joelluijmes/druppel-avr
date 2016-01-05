9ebcb2a7c353a76ae8b3904856810a7806ca7376
Joël Luijmes <joelluijmes@gmail.com>
2016-01-05 12:31:03 +0100

    Cleaned up a bit

--- util/twi/usi/twi_usi.c
+++ util/twi/usi/twi_usi.c
@@ -27,44 +27,36 @@
 #define T2_TWI    ((SYS_CLK *4700) /1000000) +1 // >4,7us
 #define T4_TWI    ((SYS_CLK *4000) /1000000) +1 // >4,0us
 
-#define SET_USI_TO_SEND_ACK()                                                                                 \
-{                                                                                                             \
-    USIDR    =  0;                                              /* Prepare ACK                         */ \
-    SDA_OUTPUT();                                               /* Set SDA as output                   */ \
-    USISR    =  (0<<USISIF)|(1<<USIOIF)|(1<<USIPF)|(1<<USIDC)|  /* Clear all flags, except Start Cond  */ \
-    (0x0E<<USICNT0);                                /* set USI counter to shift 1 bit. */ \
+#define SET_USI_TO_SEND_ACK()                                                                          \
+{                                                                                                      \
+    USIDR = 0;                                                          /* Prepare ACK */              \
+    SDA_OUTPUT();                                                       /* Set SDA as output */        \
+    USISR = (1 << USIOIF | 1 << USIPF | 1 << USIDC | 0x0E << USICNT0);  /* Clear flags | count 1 bit*/ \
 }
 
-#define SET_USI_TO_READ_ACK()                                                                                 \
-{                                                                                                             \
-    SDA_INPUT();                                                /* Set SDA as intput */                   \
-    USIDR    =  0;                                              /* Prepare ACK        */                      \
-    USISR    =  (0<<USISIF)|(1<<USIOIF)|(1<<USIPF)|(1<<USIDC)|  /* Clear all flags, except Start Cond  */ \
-    (0x0E<<USICNT0);                                /* set USI counter to shift 1 bit. */ \
+#define SET_USI_TO_READ_ACK()                                                                          \
+{                                                                                                      \
+    SDA_INPUT();                                                        /* Set SDA as intput */        \
+    USIDR = 0;                                                          /* Prepare ACK */              \
+    USISR = (1 << USIOIF | 1 << USIPF | 1 << USIDC | 0x0E << USICNT0);  /* Clear flags | count 1 bit*/ \
 }
 
-#define SET_USI_TO_TWI_START_CONDITION_MODE()                                                                                     \
-{                                                                                                                                 \
-    USICR    =  (1<<USISIE)|(0<<USIOIE)|                          /* Enable Start Condition Interrupt. Disable Overflow Interrupt.*/  \
-    (1<<USIWM1)|(0<<USIWM0)|                          /* Set USI in Two-wire mode. No USI Counter overflow hold.      */  \
-    (1<<USICS1)|(0<<USICS0)|(0<<USICLK)|              /* Shift Register Clock Source = External, positive edge        */  \
-    (0<<USITC);                                                                                                         \
-    USISR    =  (0<<USISIF)|(1<<USIOIF)|(1<<USIPF)|(1<<USIDC)|    /* Clear all flags, except Start Cond               */  \
-    (0x0<<USICNT0);                                                                                                     \
+#define SET_USI_TO_TWI_START_CONDITION_MODE()                                                          \
+{                                                                                                      \
+    USICR = (1 << USISIE | 1 << USIWM1 | 1 << USICS1);      /* Start Interrupt | TWI Mode | Ext Clk */ \
+    USISR = (1 << USIOIF | 1 << USIPF | 1 << USIDC);        /* Clear flags, except start condition */  \
 }
 
-#define SET_USI_TO_SEND_DATA()                                                                               \
-{                                                                                                            \
-    SDA_OUTPUT();                                               /* Set SDA as output                  */ \
-    USISR    =  (0<<USISIF)|(1<<USIOIF)|(1<<USIPF)|(1<<USIDC)|  /* Clear all flags, except Start Cond */ \
-    (0x0<<USICNT0);                                 /* set USI to shift out 8 bits        */ \
+#define SET_USI_TO_SEND_DATA()                                                                         \
+{                                                                                                      \
+    SDA_OUTPUT();                                           /* Set SDA as output */                    \
+    USISR = (1 << USIOIF | 1 << USIPF | 1 << USIDC);        /* Clear flags, except start condition */  \
 }
 
-#define SET_USI_TO_READ_DATA()                                                                               \
-{                                                                                                            \
-    SDA_INPUT();                                                /* Set SDA as input                   */ \
-    USISR    =  (0<<USISIF)|(1<<USIOIF)|(1<<USIPF)|(1<<USIDC)|  /* Clear all flags, except Start Cond */ \
-    (0x0<<USICNT0);                                 /* set USI to shift out 8 bits        */ \
+#define SET_USI_TO_READ_DATA()                                                                         \
+{                                                                                                      \
+    SDA_INPUT();                                            /* Set SDA as input */                     \
+    USISR = (1 << USIOIF | 1 << USIPF | 1 << USIDC);        /* Clear flags, except start condition */  \
 }
 
 #define WAIT_TRANSFER()                                                                                \
