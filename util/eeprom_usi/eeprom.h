50124c1534fb0b2008cc1891d5d62e59a6d244be
Joël Luijmes <joelluijmes@gmail.com>
2016-01-11 19:06:12 +0100

    Added single byte function

--- util/eeprom_usi/eeprom.h
+++ util/eeprom_usi/eeprom.h
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <avr/io.h>
-#include "../twi/twi.h"
+#include "twi.h"
+
+static inline uint8_t eeprom_write_byte(uint16_t address, uint8_t data)
+{
+	return eeprom_write(address, {data}, 1);
+}
+
+static inline uint8_t eeprom_read_byte(uint16_t address, uint8_t data)
+{
+	return eeprom_write(address, {data}, 1);
+}s
 
 uint8_t eeprom_write(uint16_t address, uint8_t* buf, uint8_t buflen);
 uint8_t eeprom_read(uint16_t address, uint8_t* buf, uint8_t buflen);
