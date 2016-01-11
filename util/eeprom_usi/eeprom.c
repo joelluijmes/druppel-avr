aad4f0980e98ed1262716f1a7c37916dcf93562b
Joël Luijmes <joelluijmes@gmail.com>
2016-01-11 19:56:25 +0100

    Eeprom refactored finished and working :D

--- util/eeprom_usi/eeprom.c
+++ util/eeprom_usi/eeprom.c
@@ -9,24 +9,30 @@
 
 uint8_t access_address(uint16_t address)
 {
-	uint8_t buffer[] = { address >> 8, (uint8_t)address }; 
-	return twi_master_send(I2C_ADDR_DEVICE, buffer, 2, KEEP_ALIVE) == TWST_OK;	
+	uint8_t buffer[] = { address >> 8, (uint8_t)address };
+	return twi_master_send(I2C_ADDR_DEVICE, buffer, 2, KEEP_ALIVE) == TWST_OK;
 }
 
 uint8_t eeprom_write(uint16_t address, uint8_t* buf, uint8_t buflen)
 {
 	if (!access_address(address))
+	{
+		twi_close();
 		return 0;
+	}
 
 	TWRESULT result = twi_master_send(I2C_ADDR_DEVICE, buf, buflen, CLOSE);
-	return result == TWST_OK; 
+	return result == TWST_OK;
 }
 
 uint8_t eeprom_read(uint16_t address, uint8_t* buf, uint8_t buflen)
 {
 	if (!access_address(address))
+	{
+		twi_close();
 		return 0;
+	}
 
 	TWRESULT result = twi_master_receive(I2C_ADDR_DEVICE, buf, buflen, CLOSE);
-	return result == TWST_OK; 
+	return result == TWST_OK;
 }
--- util/eeprom_usi/eeprom.h
+++ util/eeprom_usi/eeprom.h
@@ -3,16 +3,19 @@
 #include <avr/io.h>
 #include "twi.h"
 
+uint8_t eeprom_write(uint16_t address, uint8_t* buf, uint8_t buflen);
+uint8_t eeprom_read(uint16_t address, uint8_t* buf, uint8_t buflen);
+
 static inline uint8_t eeprom_write_byte(uint16_t address, uint8_t data)
 {
-	return eeprom_write(address, {data}, 1);
+	uint8_t buf[] = { data };
+	return eeprom_write(address, buf, 1);
 }
 
 static inline uint8_t eeprom_read_byte(uint16_t address)
 {
 	uint8_t buf[1];
-	return eeprom_read(address, buf, 1);
-}
+	eeprom_read(address, buf, 1);
 
-uint8_t eeprom_write(uint16_t address, uint8_t* buf, uint8_t buflen);
-uint8_t eeprom_read(uint16_t address, uint8_t* buf, uint8_t buflen);
+	return buf[0];
+}
