3e0a43cbdda9d438ae9a95ff868735fadb0858bb
Jasper <lohuis46@gmail.com>
2016-01-12 19:16:42 +0100

    remove unused files

--- bus/usi_supertiny/communication.h
+++ bus/usi_supertiny/communication.h
@@ -5,9 +5,5 @@
 #include "../../util/twi/twi.h"
 #include "../../util/eeprom/eeprom.h"
 
-
-uint8_t communication_active(); 
-void communication_send_data(); 
-
 uint8_t communication_send_data(uint8_t* data, uint8_t len);
 uint8_t communication_avaible();
